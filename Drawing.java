90a2531c9b4d9655f62aa2f7787b4f16f8518c76
matteleuteri <62861050+matteleuteri@users.noreply.github.com>
2020-03-29 22:04:14 -0400

    Add files via upload

--- Drawing.java
+++ Drawing.java
@@ -8,7 +8,7 @@ public class Drawing extends Canvas
 {
     Vector<Point> pts = new Vector<Point>();
     Vector<Point[]> lines = new Vector<Point[]>();
-    Vector<Arc> curves = new Vector<Arc>();//this one will be complicated...
+    Vector<Arc> curves = new Vector<Arc>();
 	public static void main(String[] args) 
 	{
         JFrame frame = new JFrame("My Construction");
@@ -19,8 +19,8 @@ public class Drawing extends Canvas
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Scanner scanmain = new Scanner(System.in);
         System.out.println("enter some commands...");
-        canvas.getCMD(scanmain.nextLine());
-        frame.setVisible(true);//paint is called when frame set to visible
+        //paint is called when frame set to visible
+        frame.setVisible(true);
         while(true)
         {canvas.getCMD(scanmain.nextLine());}
     }
@@ -29,19 +29,22 @@ public class Drawing extends Canvas
     	super.paint(g);
     	for(int j = 0; j < pts.size(); j++)
     	{
+    		//mark all the points
     		Point tolabel = pts.get(j);
-    		g.fillOval(tolabel.getX()-2, tolabel.getY()-2, 4, 4);
+    		//adjust size of point below
+    		int pRadius = 2;
+    		g.fillOval(tolabel.getX()-pRadius, tolabel.getY()-pRadius, 2*pRadius, 2*pRadius);
     	}
     	for(int k = 0; k < lines.size(); k++)
     	{
-    		//TODO draw all these lines in vector lines
+    		//draw all the lines
     		Point p0 = lines.get(k)[0];
     		Point p1 = lines.get(k)[1];
     		g.drawLine((int)p0.getX(), (int)p0.getY(), (int)p1.getX(), (int)p1.getY());
     	}  
     	for(int m = 0; m < curves.size(); m++)
     	{
-    		//invoke drawArc method
+    		//draw all the arcs
     		Arc myarc = curves.get(m);
     		g.drawArc(myarc.getX(), myarc.getY(), 2*myarc.getRadius(), 2*myarc.getRadius(), myarc.getStartAngle(), myarc.getArcAngle());
     	}
@@ -50,10 +53,13 @@ public class Drawing extends Canvas
     {
     	//determine what kind of command cmd is and add construct to proper vector
     	String[] tokens = cmd.split(" ");
+    	//label a single point
     	if(tokens[0].equals("label"))
     		{pts.add(new Point(tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3])));}
+    	//draw straight line between two points
     	else if(tokens[0].equals("line"))
     		{makeLine(tokens);}
+    	//draw arc about p1 through p2
     	else if(tokens[0].equals("arc"))
     		{makeArc(tokens);}
     	else
@@ -66,6 +72,7 @@ public class Drawing extends Canvas
     	Point endpoint2=null;
     	for(int l = 0; l < pts.size(); l++)
     	{
+    		//order is not important here, so no consideration is made here
     		if(pts.get(l).getName().equals(tokens[1]))
     			{endpoint1 = pts.get(l);}
     		if(pts.get(l).getName().equals(tokens[2]))
@@ -79,12 +86,14 @@ public class Drawing extends Canvas
 
     public void makeArc(String[] tokens)
     {
-    	//here is our expected tokens:
-    	//"arc point1 point2 degcw degccw"
+    	//expected tokens: "arc point1 point2 degcw degccw"
+    	//the pin point of the compass
 		Point endpoint1=null;
+		//the pencil tip is placed here initially
     	Point endpoint2=null;
     	for(int l = 0; l < pts.size(); l++)
     	{
+    		//order is important here, we have explicit start and end points
     		if(tokens[1].equals(pts.get(l).getName()))
     			{endpoint1 = pts.get(l);}
     		if(tokens[2].equals(pts.get(l).getName()))
@@ -94,22 +103,28 @@ public class Drawing extends Canvas
     		{System.out.println("point name not recognized");}
     	else
     	{
+    		//this is basic calculations for orienting ourselves
     		int xDist = endpoint1.getX()-endpoint2.getX();
     		int yDist = endpoint1.getY()-endpoint2.getY();
     		int radius = (int)Math.sqrt((xDist*xDist) + (yDist*yDist));
     		//now we get the data for the square the circle will lie within
+    		//below is the x,y coords of top left corner of the square
     		int x = endpoint1.getX()-radius;
     		int y = endpoint1.getY()-radius;
     		//below is startAngle corresponds to the 3o'clock position
     		//as is defined for the drawArc method in java
-    		int startAngle = (int)Math.toDegrees(Math.atan2(yDist, xDist))-45;
+    		int startAngle = (int)Math.toDegrees(Math.atan2(-1*yDist, xDist))+180;
     		//we draw cwangle degrees clockwise 
     		int cwAngle = Integer.parseInt(tokens[3]);
     		//and ccwangle degrees counter-clockwise 
     		int ccwAngle = Integer.parseInt(tokens[4]);
     		//to draw as a single angle, we start from minuss the cwangle
-    		curves.add(new Arc(x, y, radius, startAngle, ccwAngle));
-    		curves.add(new Arc(x, y, radius, startAngle, -1*cwAngle));
+    		//and then add the two together for a total continuous arc
+    		//the hope is this will save memory when only 1 arc is stored 
+    		curves.add(new Arc(x, y, radius, startAngle-cwAngle, cwAngle+ccwAngle));
+    		//below is how it could be done with two arcs (I like my way better)
+    		//curves.add(new Arc(x, y, radius, startAngle, ccwAngle));
+    		//curves.add(new Arc(x, y, radius, startAngle, -1*cwAngle));
     	}
     }
 }
